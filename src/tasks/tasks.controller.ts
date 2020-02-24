e5c46c56edd2b0009351ba982086212c25bcf1d3
G1ml1 <vemoleg@gmail.com>
2020-02-24 16:41:40 +0200

    create tasks module

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto) {
+    console.log('controller createTaskDto', createTaskDto);
+
+    return this.tasksService.createTask(createTaskDto);
+  }
+}
